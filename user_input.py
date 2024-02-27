68d8ced460644f6a7e730267885765b398ff2e60
BrianKvin <briankvin@gmail.com>
2024-02-27 11:38:02 +0300

    Python: data types

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where do you come from? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
