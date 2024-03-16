d0605c4b36631bc8cce97058a7cbb7762adcee0f
BrianKvin <briankvin@gmail.com>
2024-03-16 16:33:27 +0300

    A turple that prints books

---
+++ books.py
@@ -0,0 +1,4 @@
+books = ("The River and the source", "AN enemy of the people", "Intermediate Python", "Data science for beginners", "Machine learning the right way")
+print("My favorite books:")
+for bk in books:
+    print(bk)
