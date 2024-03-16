28440baf7876816859b65e662007b2534e329a21
BrianKvin <briankvin@gmail.com>
2024-03-16 17:34:46 +0300

    A dictionary of books

--- books.py
+++ books.py
@@ -1,3 +1,8 @@
+""" 
+Create a tuple containing the names of five of your favorite books. 
+Then, use a for loop to print each book name on a separate line.
+"""
+
 books = ("The River and the source", "AN enemy of the people", "Intermediate Python", "Data science for beginners", "Machine learning the right way")
 print("My favorite books:")
 for bk in books:
